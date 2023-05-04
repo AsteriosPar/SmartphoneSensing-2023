76925b88e8fa780454e9ca420359b084dc1da42b
sergio <A.parlitsis@student.tudelft.nl>
2023-05-04 18:16:57 +0200

    Fixed: All permissions are checked appropriatelly

--- app/src/main/java/com/tudelft/indoorlocalizationapp/TrainActivity.java
+++ app/src/main/java/com/tudelft/indoorlocalizationapp/TrainActivity.java
@@ -7,19 +7,26 @@ import android.Manifest;
 import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.location.LocationManager;
 import android.net.wifi.ScanResult;
+import android.net.wifi.WifiInfo;
 import android.net.wifi.WifiManager;
 import android.os.Bundle;
+import android.provider.Settings;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class TrainActivity extends AppCompatActivity implements View.OnClickListener {
-
-    private WifiManager wifiManager;
-    private TextView txt_train;
+    private ListView txt_train;
+    ArrayList<String> listItems = new ArrayList<String>();
+    ArrayAdapter<String> adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,37 +36,68 @@ public class TrainActivity extends AppCompatActivity implements View.OnClickList
         String cell = intent.getStringExtra("key");
 
         Button btn_train = (Button) findViewById(R.id.btn_train);
-        txt_train = (TextView) findViewById(R.id.text_train);
+        txt_train = (ListView) findViewById(R.id.text_train);
         TextView txt_train_title = findViewById(R.id.text_train_title);
+
+        // Set Title with the appropriate cell name.
         txt_train_title.setText("Go to location " + cell + " and press <Scan> to detect WIFI signal");
 
+        // Create an empty ListView
+        adapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_1,
+                listItems);
+        txt_train.setAdapter(adapter);
+
+        // Connect button to listener
         btn_train.setOnClickListener(this);
     }
 
     @Override
     public void onClick(View v) {
         // Set wifi manager.
-        wifiManager = (WifiManager) getApplicationContext().getSystemService(Context.WIFI_SERVICE);
-        // Start a wifi scan.
-        wifiManager.startScan();
-        // Store results in a list.
+        WifiManager wifiManager = (WifiManager) getApplicationContext().getSystemService(Context.WIFI_SERVICE);
+        // Set location manager (Location is also necessary)
+        LocationManager locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+
+        // Check if the user has given permission for WIFI scan
         if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            // TODO: Consider calling
-            //    ActivityCompat#requestPermissions
-            // here to request the missing permissions, and then overriding
-            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-            //                                          int[] grantResults)
-            // to handle the case where the user grants the permission. See the documentation
-            // for ActivityCompat#requestPermissions for more details.
+            Toast toast = Toast.makeText(getApplicationContext(), "Permission for WiFi scan not granted", Toast.LENGTH_SHORT);
+            toast.show();
             return;
         }
-        List<ScanResult> scanResults = wifiManager.getScanResults();
-        findViewById(R.id.bg_train).setAlpha(0);
-        // Write results to a label
-        for (ScanResult scanResult : scanResults) {
-            txt_train.setText(txt_train.getText() + "\n\tBSSID = "
-                    + scanResult.BSSID + "    RSSI = "
-                    + scanResult.level + "dBm");
+
+        if (!locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
+            Toast toast = Toast.makeText(getApplicationContext(), "Location Service is required for this action!", Toast.LENGTH_LONG);
+            toast.show();
+            Intent myIntent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+            startActivity(myIntent);
+
+        } else {
+            // Start a wifi scan.
+            wifiManager.startScan();
+
+            // Store results in a list.
+            List<ScanResult> scanResults = wifiManager.getScanResults();
+
+            if (scanResults.isEmpty()) {
+                Toast toast = Toast.makeText(getApplicationContext(), "No WiFi APs detected!", Toast.LENGTH_SHORT);
+                toast.show();
+            } else {
+                // Remove the background.
+                findViewById(R.id.bg_train).setAlpha(0);
+            }
+            // Write results to a label
+            for (ScanResult scanResult : scanResults) {
+//                txt_train.setText(
+//                        txt_train.getText() +
+//                                "\n\tBSSID = " +
+//                                scanResult.BSSID +
+//                                "\n\tRSSI = " +
+//                                scanResult.level + "dBm"
+//                );
+                adapter.add("\n\tBSSID = " + scanResult.BSSID + "\n\tRSSI = " + scanResult.level + "dBm");
+            }
         }
+
     }
 }
